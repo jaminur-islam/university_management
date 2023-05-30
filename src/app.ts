c44479770728200db5779e3e1c3d046b265ab341
jaminur-islam <jaminurislam350@gmail.com>
2023-05-30 12:09:21 +0600

    .

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,16 @@
 import express, { Application, Request, Response } from 'express'
-import cors from 'cors';
-const app: Application = express();
-
-const port = 5000
+import cors from 'cors'
+const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-
 app.use(
   express.urlencoded({
     extended: true,
   })
 )
 
-app.use('/api/v1/user', 
-
-
-
-(req:Request, res:any) => {
+app.use('/api/v1/user', (req: Request, res: Response) => {
   res.send('ok')
 })
 
